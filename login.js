dc54d50359593598f6095c56257e26d436c18026
cmu-tcas <taweesak.anakkhaphan@gmail.com>
2025-01-16 17:12:03 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,49 +1,3 @@
-// function login() {
-//     const username = document.getElementById('username').value.trim();
-//     const password = document.getElementById('password').value.trim();
-
-//     if (!username || !password) {
-//         Swal.fire({
-//             icon: 'error',
-//             title: 'Error',
-//             text: 'กรุณากรอกข้อมูลให้ครบถ้วน'
-//         });
-//         return false;
-//     }
-
-//     fetch('users.json')
-//         .then(response => response.json())
-//         .then(users => {
-//             const user = users.find(user => user.username === username && user.password === password);
-//             if (user) {
-//                 // Save user data in localStorage for future use
-//                 localStorage.setItem('user', JSON.stringify(user));
-//                 Swal.fire({
-//                     icon: 'success',
-//                     title: 'Success',
-//                     text: 'ล็อกอินสำเร็จ'
-//                 }).then(() => {
-//                     window.location.href = 'vote.html'; // Redirect to the vote page
-//                 });
-//             } else {
-//                 Swal.fire({
-//                     icon: 'error',
-//                     title: 'Error',
-//                     text: 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง'
-//                 });
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Error fetching users:', error);
-//             Swal.fire({
-//                 icon: 'error',
-//                 title: 'Error',
-//                 text: 'เกิดข้อผิดพลาด กรุณาลองอีกครั้ง'
-//             });
-//         });
-
-//     return false; // Prevent form submission
-// }
 function login() {
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
@@ -57,7 +11,7 @@ function login() {
         return false;
     }
 
-    fetch('users.json')
+    fetch('./users.json')
         .then(response => response.json())
         .then(users => {
             const user = users.find(user => user.username === username && user.password === password);
@@ -73,7 +27,7 @@ function login() {
                     title: 'Success',
                     text: 'ล็อกอินสำเร็จ'
                 }).then(() => {
-                    window.location.href = 'vote.html'; // Redirect to vote page
+                    window.location.href = './vote.html'; // Redirect to vote page
                 });
             } else {
                 Swal.fire({
