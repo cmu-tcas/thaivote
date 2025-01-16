1d55b9860dcbb586ce58920542aa7a5e23a7336f
cmu-tcas <taweesak.anakkhaphan@gmail.com>
2025-01-16 15:11:32 +0700

    Update login.js

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/index', (req, res) => {
+app.post('/vote.html', (req, res) => {
   const { username, password } = req.body;
 
   // อ่านไฟล์ ect.json
